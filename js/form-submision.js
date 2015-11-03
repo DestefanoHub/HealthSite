7aad40f1a8a0f10e22440748f0ad9425fd805373
andrew <elitehunter92@gmail.com>
2015-11-03 15:37:12 -0500

    Added: - form submission works except that the result answers are the radio button value (1-4) instead of the text of the answer

---
+++ js/form-submision.js
@@ -0,0 +1,21 @@
+/**
+ * Created by andrew on 11/3/15.
+ */
+$(document).ready(function(){
+    $("form").submit(function(event){
+        event.preventDefault();
+        var form = $("form").serializeArray();
+        $.post("../php/form.php", {form: form},
+            function(data){
+                $.each(data, function(key, value){
+                    if(value.value == true){
+                        var html = "<p class='correct'>" + value.name + "<br />" + value.answer + "<br /> </p>";
+                    } else{
+                        var html = "<p class='incorrect'>" + value.name + "<br />" + value.answer + "<br /> </p>";
+                    }
+                    $("div#result").append(html);
+
+                });
+            });
+    });
+});
