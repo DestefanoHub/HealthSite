88a7fdfd4d694dc68b3052a2ddd42021bd88d365
andrew <elitehunter92@gmail.com>
2015-11-09 22:40:30 -0500

    Added: - fixed up most of index.html

---
+++ js/index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by andrew on 11/9/15.
+ */
+$(document).ready(function(){
+   $("button#aid-kit").click(function(){
+       location.assign("../html/kit.html");
+   });
+    $("button#help").click(function(){
+        location.assign("#");
+    });
+    $("button#info").click(function(){
+        location.assign("../html/Edu_Injuries.html");
+    });
+    $("button#911").click(function(){
+        location.assign("#");
+    });
+});
