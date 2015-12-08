a6737a7e54336076b837797517637b9d7d0e287c
andrew <elitehunter92@gmail.com>
2015-12-08 12:28:19 -0500

    Added: - fixed some image tag errors - added the extra information sections (dynamic) - added citations for extra info - abrasions, lacerations, and punctures have the extra info

--- js/slideout.js
+++ js/slideout.js
@@ -1,7 +1,19 @@
 jQuery(function ($) {
     $(".hasSlideOut").on("click", function(){
         setTimeout(function(){
-            $("div.slideout").slideDown("slow", function(){});
+            $("button.slideout_button").slideDown("slow", function(){});
         }, 3000);
+    });
+
+    $("button.slideout_button").on("click", function(){
+        var pageName = $(this).attr("id") + "_extra.html";
+        var sectionName = $(this).attr("id") + "_extra";
+        var divSection = $("div[id='"+ sectionName +"']");
+        console.log(divSection);
+        $.get("../page-content/" + pageName, function (pageData) {
+            console.log(pageData);
+            divSection.html(pageData);
+            divSection.css("display", "block");
+        });
     });
 });
